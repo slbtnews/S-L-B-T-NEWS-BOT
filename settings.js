2d1e365e5628541aecede8dca89fdea771b87c89
slbtnews <113825664+slbtnews@users.noreply.github.com>
2022-09-18 14:51:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -47,9 +47,9 @@ global.owner = ['94755744843']
 global.ownertag = '94755744843' //ur tag number
 global.botnumber = '94755744843' //Add your Bot Number For Log
 global.botname = 'S.L.B.T NEWS MUSIC BOT' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
-global.sc = 'https://github.com/darkalphaxteam' //script link
+global.ig = "https://chat.whatsapp.com/Ha1AgHnsx86AnETJKj8mqO*" //your theme url which will be displayed on whatsapp
+global.myweb = "https://chat.whatsapp.com/Ha1AgHnsx86AnETJKj8mqO" //ur website to be displayed
+global.sc = 'https://chat.whatsapp.com/Ha1AgHnsx86AnETJKj8mqO' //script link
 global.linkz = 'https://youtu.be/uzMbW5v9QLE'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
@@ -60,7 +60,7 @@ global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
 global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
+global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...slbtnewsgroupaddmin\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
 
 //BOT MEDIAS
 global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
